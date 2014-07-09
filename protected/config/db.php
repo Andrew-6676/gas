59a4a8c66c83fb4e2534da9985f729c0bfe9337c
Andrew_250 <Andrew@vitebsk.oblgas.by>
2014-07-09 16:14:41 +0300

    исключить db.php из коммитов

--- protected/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-			//'class'=>'CDbConnection',
-			'connectionString'=>'mysql:host=localhost;dbname=vg_site',
-            'username'=>'root',
-            'password'=>'root',
-            'charset'=>'utf8'
-			//'tablePrefix' => 'vgs_',
-		);
