313def5ea877d05451f180354a0600b65203db78
dios <dios@kadry2>
2014-08-06 12:48:02 +0300

    view

--- protected/controllers/page/contactAction.php
+++ protected/controllers/page/contactAction.php
@@ -4,8 +4,11 @@ class contactAction extends CAction /* pageController */
 {
     public function run()
 	{
+		$criteria = new CDbCriteria;
+		$criteria->order ='sort, id';
+		$criteria->addCondition("tel!=''");
 
-		$q_model = Contact::model()->findAll("tel!=''");
+		$q_model = Contact::model()->findAll($criteria); /*"tel!=''"*/
 
 			// рендерим страницу
 		$this->controller->render('contact',
