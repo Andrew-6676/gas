d3ddb5669632e90da40c9b5142986a95404bd3ea
dios <dios@kadry2>
2014-08-06 12:34:40 +0300

    view

--- protected/views/page/contact.php
+++ protected/views/page/contact.php
@@ -18,30 +18,38 @@ $this->addCss('page/contact.css');
 	<div class="border">
 
 	<table class="contact-table">	
-	<colgroup>
-		<col style="width:21%"></col>
-		<col style="width:40%;"></col>
-		<col style="width:29%"></col>
-		<col style="width:10%"></col>
-	</colgroup>
-	<thead>
-		<tr>
-			<td>Служба</td>
-			<td>Должность</td>
-			<td>ФИО</td>
-			<td>Телефон</td>
-		</tr>
-	</thead>
-		<?php
-		foreach($q_model as $values){
-			?>
-			<tr><td <? if($values->sort==0){echo "class=\"boss\"";}?>><?php echo $values->otdel;?></td>
-				<td <? if($values->sort==0){echo "class=\"boss\"";}?>><?php echo $values->dolzn;?></td>
-				<td class="secondName <? if($values->sort==0){echo "boss";}?>"><?php echo$values->fio;?></td>
-				<td <? if($values->sort==0){echo "class=\"boss\"";}?>><?php echo $values->tel;?></td>
+		<colgroup>
+			<col style="width:21%"></col>
+			<col style="width:40%;"></col>
+			<col style="width:29%"></col>
+			<col style="width:10%"></col>
+		</colgroup>
+		<thead>
+			<tr>
+				<th>Служба</th>
+				<th>Должность</th>
+				<th>ФИО</th>
+				<th>Телефон</th>
 			</tr>
-		<?php }
-		?>
+		</thead>
+			<?php
+			foreach($q_model as $values){
+				?>
+				<tr><td <? if($values->sort==0){echo "class=\"boss\"";}?>><?php echo $values->otdel;?></td>
+					<td <? if($values->sort==0){echo "class=\"boss\"";}?>><?php echo $values->dolzn;?></td>
+					<td class="secondName <? if($values->sort==0){echo "boss";}?>"><?php echo$values->fio;?></td>
+					<td <? if($values->sort==0){echo "class=\"boss\"";}?>><?php echo $values->tel;?></td>
+				</tr>
+			<?php }
+			?>
+		<tfoot>
+			<tr>
+				<th>Служба</th>
+				<th>Должность</th>
+				<th>ФИО</th>
+				<th>Телефон</th>
+			</tr>
+		</tfoot>
 	</table>
 
 	</div>
