13a6afda1e320690dddeea507e160ca9cc3901d6
dios <dios@kadry2>
2014-08-06 13:58:48 +0300

    job

--- protected/views/page/vakansii.php
+++ protected/views/page/vakansii.php
@@ -1,1 +1,28 @@
-vacansii
+<?php
+$this->breadcrumbs = array(
+			//'Djghjc'=>array('site/page/'),
+			'Контакты',
+		);
+$this->addCss('page/page.css');
+$this->addCss('page/contact.css');
+
+/*print_r($q_model);*/
+
+
+
+?>
+
+<div class='page'>
+	<h1>Контакты</h1>
+
+	<div class="border">
+
+	<?php
+		foreach($q_model as $values){
+			$values->name.'<br>';
+		}
+	?>
+
+	</div>
+
+</div>
