358bb2f81e3bd915ff9ff717e25da1be9527692c
dios <dios@kadry2>
2014-08-06 10:27:49 +0300

    Изменена страница контакты. Добавлены стили

--- protected/views/page/contact.php
+++ protected/views/page/contact.php
@@ -1,8 +1,30 @@
 <?php
-
+$this->breadcrumbs = array(
+			//'Djghjc'=>array('site/page/'),
+			'Контакты',
+		);
+$this->addCss('page/page.css');
+$this->addCss('page/contact.css');
 
 /*print_r($q_model);*/
+
+
+
+?>
+
+<div class='page'>
+<h1>Контакты</h1>
+
+<div class="border">
+	
+<?php
 
 foreach($q_model as $values){
 echo $values->id.'<br>';
 }
+
+?>
+
+</div>
+
+</div>
