0c1ca82dfb2cc1fcb53884cc2a0ba2a613e23b25
Andrew_250 <Andrew@vitebsk.oblgas.by>
2014-09-15 16:03:51 +0300

    скрипт добавления подписей к картинкам усовершенствован

--- js/page/podpis.js
+++ js/page/podpis.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 	//alert('podpis');
 
 	var images = $('.content img');
+	img_descr(images);
 
+})
+
+
+function img_descr(images) {
 	images.each(function(i){
 		if ($(this).attr('alt')!=undefined && $(this).attr('alt')!='') {
 			//alert($(this).height());
@@ -11,6 +16,11 @@ $(document).ready(function(){
 			//alert($(div).text());
 			$(div).width($(this).width()-2);
 			$(div).offset({top:$(this).offset().top+$(this).height()-3, left:$(this).offset().left });
+			if ($(div).height() > 20) {
+				//alert($(div).height());
+				$(this).css({'margin-bottom': 6+$(div).height()});
+			}
 		}
 	})
-})
+
+}
